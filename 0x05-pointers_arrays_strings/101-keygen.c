d562ebca6dd5c5baf272757a5f4c076359392fe4
letschangeAfrica <ismaelkagou@gmail.com>
2023-08-21 14:55:51 -0700

    generate random valid passwords

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,6 +10,7 @@
  */
 int main(void)
 {
+
 int pass[100];
 int i, sum, n;
 sum = 0;
@@ -19,7 +20,7 @@ for (i = 0; i < 100; i++)
 pass[i] = rand() % 78;
 sum += (pass[i] + '0');
 putchar(pass[i] + '0');
-if ((2772 - sum) - '0' < 78)
+if ((2772 - sum) -'0' < 78)
 {
 n = 2772 - sum - '0';
 sum += n;
