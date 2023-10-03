63364aca5bc73fb5c577162992c0a03cb80d0b92
letschangeAfrica <ismaelkagou@gmail.com>
2023-10-03 07:52:42 -0700

    new try

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fd;
+int nletters;
+int nwr;
+if (!filename)
+return (-1);
+fd = open(filename, 0_CREAT | O_WRONGLY | O_TRUNC, 0600);
+if (fd == -1)
+return (-1);
+if (!text_content)
+text_content = "";
+for (nletters = 0; text_content[nletters]; nletters++)
+;
+nwr = write(fd, text_content, nletters);
+if (rwr == -1)
+return (-1);
+close(fd);
+return (1);
+}
