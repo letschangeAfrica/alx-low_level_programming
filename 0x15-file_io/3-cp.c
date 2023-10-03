00f79a48f79d89c54d63b1eab6231b0bd74e82e8
letschangeAfrica <ismaelkagou@gmail.com>
2023-10-03 14:34:33 -0700

    change variable

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,7 +27,7 @@ exit(99);
  * @argv: arguments vector.
  * Return: Always 0.
  */
-int main(int argc, char char *argv[])
+int main(int argc, char *argv[])
 {
 int file_from, file_to, err_close;
 ssize_t nchars, nwr;
@@ -38,7 +38,7 @@ dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 exit(97);
 }
 file_from = open(argv[1], O_RDONLY);
-file_to = open(argv[2], 0_CREAT | O_WRONLY | O_TRUNC §O_APPEND, 0664);
+file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 error_file(file_from, file_to, argv);
 nchars = 1024;
 while (nchars == 1024)
@@ -59,7 +59,7 @@ exit(100);
 err_close = close(file_to);
 if (err_close == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", filr_from);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 exit(100);
 }
 return (0);
