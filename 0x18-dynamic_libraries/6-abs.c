f56244462711d654d63705cc1c3f29c61e52097c
letschangeAfrica <ismaelkagou@gmail.com>
2024-01-13 11:56:44 -0800

    stressing

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs- Function that computes the absolute value of an integer.
+ * @n: integer value.
+ *
+ * Return: 0 or 1 depending on the answer
+*/
+
+int _abs(int n)
+{
+	return (abs(n));
+}
