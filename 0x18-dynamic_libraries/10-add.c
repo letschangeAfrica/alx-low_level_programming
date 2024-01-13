d8b9d0e04bd18c074b11236bf797927b0ee133dd
letschangeAfrica <ismaelkagou@gmail.com>
2024-01-13 12:59:05 -0800

    mama

---
+++ 0x18-dynamic_libraries/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * add - A function that adds two integers.
+ * @a: integer number
+ * @b: integer number
+ *
+ * Return: Result of the addition.
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x18-dynamic_libraries/11-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add - A function that adds two integers.
+ * @a: integer number
+ * @b: integer number
+ *
+ * Return: Result of the addition.
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
